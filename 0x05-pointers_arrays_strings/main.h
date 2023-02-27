a8ea560a2bf19a468d0913ea090db500dc9ad7f6
Ntim Elijah Mintah <ntimelijahmintah@gmail.com>
2023-02-27 05:29:14 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIn_H
 int _putcher(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
+#endif
