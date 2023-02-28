5ae2edbadacfbdf4b4456fcdbb1fa2a5b1c59673
Ntim Elijah Mintah <ntimelijahmintah@gmail.com>
2023-02-28 02:14:16 -0800

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,14 @@ void print_array(int *a, int n)
 {
 	int j;
 
-	for (j = 0; < n; j++)
+	for (j = 0; j < n; j++)
 	{
 	printf("%d", a[j]);
 	if (j != (n - 1))
 	{
 	printf(", ");
 	}
+	}
 	printf("\n");
+
+}
